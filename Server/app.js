d37dcd47e432428117178966e31a029ed3e6a251
Terry <rkdxogns1234@daum.net>
2017-03-28 00:46:04 -0700

    Added comments to server side and added dependancy libraries into package.json

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,19 @@
-//app.js
+/*------------------------------------------------------------------------------------------------------------------
+* SOURCE FILE: app.js
+*
+* PROGRAM: node.js server for AndroidGPS
+*
+* DATE: March 27, 2017
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+*
+* PROGRAMMER: Terry Kang
+*
+* NOTES:
+*   This is the node.js server that that uses socket-io to establish socket connection to client 
+*	and receive coordinates of client's locations. Then, server also serves as http server to show the goole map 
+*	and plot the coordinates recevied from the clients via express library.
+----------------------------------------------------------------------------------------------------------------------*/
 var express = require('express');  
 var app = express();  
 var session = require('express-session')({
@@ -6,15 +21,36 @@ var session = require('express-session')({
     resave: true,
     saveUninitialized: true
 });
-//var sharedsession = require("express-socket.io-session");
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 var _ = require('lodash-node');
 var fs = require('fs');
-var bodyParser     =        require("body-parser");
-//Here we are configuring express to use body-parser as middle-ware.
+var bodyParser = require("body-parser");
 
-// Authentication and Authorization Middleware
+
+app.use(express.static(__dirname + '/node_modules')); 
+app.use(session);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: auth
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: function(req, res, next)
+*
+* RETURN: originally requested page if authorized, otherwise, render login page
+*
+* NOTES:
+*   authentication middleware that checks if this session is authorized and if it is, render  originally requested page,
+*	otherwise, render login page.
+----------------------------------------------------------------------------------------------------------------------*/
 var auth = function(req, res, next) {
   if (req.session && req.session.athenticated)
     return next();
@@ -23,24 +59,89 @@ var auth = function(req, res, next) {
 };
 
 var users = [];
+//User's login info (username and password)
 var loginData = JSON.parse(fs.readFileSync(__dirname + '/loginData.json', 'utf8'));
 console.log(loginData);
 
-app.use(express.static(__dirname + '/node_modules')); 
-app.use(session);
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
 
-//io.use(sharedsession(session));
-
-
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.get('/')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.get('/', auth, function(req, res,next)
+*
+* RETURN: index page if authorized, otherwise, render login page
+*
+* NOTES:
+*   Render root page after checking authentication via middleware
+----------------------------------------------------------------------------------------------------------------------*/
 app.get('/', auth, function(req, res,next) {  
 	res.sendFile(__dirname + '/index.html');
 });
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.get('/login')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.get('/login', function(req, res,next)
+*
+* RETURN: render login page
+*
+* NOTES:
+*   Render login page
+----------------------------------------------------------------------------------------------------------------------*/
 app.get('/login', function(req, res,next) {  
 	res.sendFile(__dirname + '/login.html');
 });
-app.post('/doLogin',function(req,res){
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.get('/doLogin')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.get('/doLogin', auth, function(req, res,next)
+*
+* RETURN: render login page
+*
+* NOTES:
+*   Render index page if user access to /doLogin by mistake
+----------------------------------------------------------------------------------------------------------------------*/
+app.get('/doLogin', auth, function(req, res,next) {  
+	res.sendFile(__dirname + '/index.html');
+});
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.post('/doLogin')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.post('/doLogin',function(req,res)
+*
+* RETURN: index page if authorized, otherwise, render login page
+*
+* NOTES:
+*   Receives username and password as parameters via post request and then if recevied username and password match with
+*	the login information stored, update session as authorized and then render index page. otherwise, render login page.
+----------------------------------------------------------------------------------------------------------------------*/
+app.post('/doLogin',function(req,res) {
 	if(req.body.length == 0 || !req.body.username || !req.body.password){
 		res.sendFile(__dirname + '/login.html');
 		return;
@@ -57,6 +158,24 @@ app.post('/doLogin',function(req,res){
 		res.sendFile(__dirname + '/login.html');
 	}
 });
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.get('/locations')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.get('/locations', function(req, res,next)
+*
+* RETURN: the coordinates data received from clients
+*
+* NOTES:
+*   Called when receives get request of '/locations' via http. Read a json file that stores the coordinates received
+*	from the client applications, then returns it.
+----------------------------------------------------------------------------------------------------------------------*/
 app.get('/locations', function(req, res,next) { 
 
 	var data = '';
@@ -72,33 +191,85 @@ app.get('/locations', function(req, res,next) {
 		res.json(data);
 	});
 });
-app.get('/:file', function(req, res, next) {  
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.get('/:file')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.get('/:file', auth, function(req, res, next)
+*
+* RETURN: the coordinates data received from clients
+*
+* NOTES:
+*   This is called when requested to access file in the server's directory from server web page.
+----------------------------------------------------------------------------------------------------------------------*/
+app.get('/:file', auth, function(req, res, next) {  
 	res.sendFile(__dirname + '/' + req.params.file );
 });
-app.get('/:dir/:file', function(req, res, next) {  
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: app.get('/:dir/:file')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: app.get('/:dir/:file', auth, function(req, res, next)
+*
+* RETURN: the coordinates data received from clients
+*
+* NOTES:
+*   This is called when requested to access file in the server's directory from server web page.
+----------------------------------------------------------------------------------------------------------------------*/
+app.get('/:dir/:file', auth, function(req, res, next) {  
 	console.log(req.params.file);
 	res.sendFile(__dirname + '/' + req.params.dir + '/' + req.params.file);
 });
 
+
+/*------------------------------------------------------------------------------------------------------------------
+* FUNCTION: io.on('connection')
+*
+* DATE: March 27, 2017
+* REVISIONS: (Date and Description)
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+* PROGRAMMER: Terry Kang
+*
+* INTERFACE: io.on('connection', function (socket)
+*				socket : the socket information
+*
+* RETURN: void
+*
+* NOTES:
+*   This is the listener for socket connection via socket.io. This function waits for socket connection from client.
+*	After making the socket connection, it waits for and handles all the messages between server and client via the socket.
+----------------------------------------------------------------------------------------------------------------------*/
 io.on('connection', function (socket) {
-	socket.on('connected', function (device) {
-		// if this socket is already connected,
-		// send a failed login message
-		if (_.findIndex(users, { socket: socket.id }) !== -1) {
-			socket.emit('connect_error', 'You are already connected.');
-		}
-
-	    if (_.findIndex(users.device, { id: device.id }) !== -1) {
-			socket.emit('connect_error', 'This name already exists.');
-		    return; 
-		}
-
-	    users.push({ 
-		    device: device,
-		    socket: socket.id
-		});
-	});
-
+	
+	/*------------------------------------------------------------------------------------------------------------------
+	* FUNCTION: socket.on('disconnect')
+	*
+	* DATE: March 27, 2017
+	* REVISIONS: (Date and Description)
+	*
+	* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+	* PROGRAMMER: Terry Kang
+	*
+	* INTERFACE: socket.on('disconnect', function ()
+	*
+	* RETURN: void
+	*
+	* NOTES:
+	*   Called when user disconnect. Remove the user from the array of current users.
+	----------------------------------------------------------------------------------------------------------------------*/
 	socket.on('disconnect', function (){
 		var index = _.findIndex(users, { socket: socket.id });
 		if(index !== -1){
@@ -107,6 +278,24 @@ io.on('connection', function (socket) {
 		}
 	});
 
+	/*------------------------------------------------------------------------------------------------------------------
+	* FUNCTION: socket.on('login')
+	*
+	* DATE: March 27, 2017
+	* REVISIONS: (Date and Description)
+	*
+	* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+	* PROGRAMMER: Terry Kang
+	*
+	* INTERFACE: socket.on('login', function (userdata)
+	*				userdata : username and password
+	*
+	* RETURN: void
+	*
+	* NOTES:
+	*   Called when user requests to login and receives username and password. if recevied username and password match with
+	*	the login information stored, save the user data into the array of current user and send success message to the client.
+	----------------------------------------------------------------------------------------------------------------------*/
 	socket.on("login", function(userdata){
 		console.log("login");
 		if(!userdata.username || !userdata.password){
@@ -139,38 +328,43 @@ io.on('connection', function (socket) {
 		    socket: socket.id
 		});
 		socket.emit('login_success', 'Succefully logged in.');
-		console.log("login_success");
+		console.log("login_success : " + userdata.username);
 	});
 
-	socket.on('location', function (data){
+	/*------------------------------------------------------------------------------------------------------------------
+	* FUNCTION: socket.on('location')
+	*
+	* DATE: March 27, 2017
+	* REVISIONS: (Date and Description)
+	*
+	* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+	* PROGRAMMER: Terry Kang
+	*
+	* INTERFACE: socket.on('location', function (data)
+	*				data : locations data (deviceID, deviceIP, username, latitude, longitude, time, accuracy)
+	*
+	* RETURN: void
+	*
+	* NOTES:
+	*   Called when client sends the coordinates of current location. Stores it into a json file to be used 
+	*	for map page to plot the coordinates on the google map.
+	----------------------------------------------------------------------------------------------------------------------*/
+	socket.on('location', function (newLocation){
 		console.log("location received");
 		var index = _.findIndex(users, { socket: socket.id });
 		if(index == -1){
 			console.log('Connection lost, reconnect.');
 		}
 		
-		var deviceID  = data.deviceID;
-		var deviceIP  = data.deviceIP;
-		var username  =	data.username;
-		var latitude  = data.latitude;
-		var longitude = data.longitude;
-		var time      = data.time;
-		//var ip        = location.ip;
-		
 		if (!fs.existsSync(__dirname + '/locations.json')) { 
         	fs.writeFileSync(__dirname + '/locations.json', '[]');
 		} 
+
 		var file = fs.readFileSync(__dirname + '/locations.json');
 		var object = JSON.parse(file);
-		var index = _.findIndex(object, { username: data.username });
-		var newLocation = {
-			"deviceID":deviceID,
-			"deviceIP":deviceIP,
-			"latitude":latitude,
-			"longitude":longitude,
-			"time":time
-		};
-		if(index == -1){
+		console.log(newLocation);
+		var index2 = _.findIndex(object, { username: users[index].username });
+		if(index2 == -1){
 			object.push(
 				{
 					"username" : username,
@@ -178,7 +372,7 @@ io.on('connection', function (socket) {
 				}
 			);
 		}else{
-			object[index].locations.push(newLocation);
+			object[index2].locations.push(newLocation);
 		}
 		var newJson = JSON.stringify(object);
 		fs.writeFileSync('./locations.json', newJson);
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,44 @@
+/*------------------------------------------------------------------------------------------------------------------
+* SOURCE FILE: index.js
+*
+* PROGRAM: google maps for AndroidGPS
+*
+* DATE: March 27, 2017
+*
+* DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+*
+* PROGRAMMER: Terry Kang
+*
+* NOTES:
+*   This is the js file that is used for index.html. Initializes google maps, request the locations to server, 
+*   receives the requested locations from server and plots the locations on the map.
+----------------------------------------------------------------------------------------------------------------------*/
     var map;
     var interval;
     var bounds;
     var users = [];
     var infowindow;
-    var markerColor = {"start": "blue", "recent": "red"};
+    var markerColor = {"start": ["blue", "blue", "blue", "blue"], "recent": ["orange", "green", "purple", "gray"]};
+
     var lineColor = ["orange", "green", "purple", "gray"];
-
-
-    function loadMap(){
+    
+    /*------------------------------------------------------------------------------------------------------------------
+    * FUNCTION: loadMap
+    *
+    * DATE: March 27, 2017
+    * REVISIONS: (Date and Description)
+    *
+    * DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+    * PROGRAMMER: Terry Kang
+    *
+    * INTERFACE: function loadMap()
+    *
+    * RETURN: void
+    *
+    * NOTES:
+    *   Initializes and shows the google maps on the html page.
+    ----------------------------------------------------------------------------------------------------------------------*/
+    function loadMap() {
       var styledMap = [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
 
       var mapOptions = {
@@ -29,6 +60,26 @@
       infowindow = new google.maps.InfoWindow();
     }
 
+    /*------------------------------------------------------------------------------------------------------------------
+    * FUNCTION: addMarker
+    *
+    * DATE: March 27, 2017
+    * REVISIONS: (Date and Description)
+    *
+    * DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+    * PROGRAMMER: Terry Kang
+    *
+    * INTERFACE: function addMarker(index, location, username, markerName)
+    *                           index : index of user array
+    *                           location : location information  (deviceID, deviceIP, latitude, longitude, time, accuracy)
+    *                           username : user's name of this location
+    *                           markerName : 'recent' location or 'start' location
+    *
+    * RETURN: void
+    *
+    * NOTES:
+    *   Adds a marker on the google map.
+    ----------------------------------------------------------------------------------------------------------------------*/
     function addMarker(index, location, username, markerName){
         var marker;
         var pos = new google.maps.LatLng(location.latitude, location.longitude);
@@ -40,12 +91,20 @@
         marker = new google.maps.Marker({
             position: pos,
             map: map,
-            icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/' + markerColor[markerName] + '-dot.png'
+            icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/' + markerColor[markerName][index] + '-dot.png'
         });
         google.maps.event.addListener(marker, 'click', (function(marker) {
             return function() {
-                infowindow.setContent("<h2>" + markerName + "</h2><b>UserName</b> : " + username + "<br><b>Time</b> : " + dateString + "<br><b>deviceIP</b> : " + deviceIP);
-                infowindow.open(map, marker);
+                infowindow.setContent(
+                    "<div>" +
+                        "<h2>" + markerName + "</h2>" + 
+                        "<b>UserName</b> : " + username + "<br>" + 
+                        "<b>Time</b> : " + dateString + "<br>" + 
+                        "<b>deviceIP</b> : " + deviceIP + "<br>" + 
+                        "<b>lat</b> : " + location.latitude + "<br>" + 
+                        "<b>long</b> : " + location.longitude + 
+                    "</div>");
+				infowindow.open(map, marker);
             }
         })(marker));
         if(users[index].hasOwnProperty(markerName))
@@ -53,6 +112,24 @@
         users[index][markerName] = marker;
     }
 
+    /*------------------------------------------------------------------------------------------------------------------
+    * FUNCTION: loadLocations
+    *
+    * DATE: March 27, 2017
+    * REVISIONS: (Date and Description)
+    *
+    * DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+    * PROGRAMMER: Terry Kang
+    *
+    * INTERFACE: function loadLocations()
+    *
+    * RETURN: void
+    *
+    * NOTES:
+    *   Requests all of the sotred locations to the server asychronously via aja(ajax).
+    *   Once it receives the locations from the server, it loops through the received locations 
+    *   and plot the path of user's movement on the mapp and also makers for starting location and recent location.
+    ----------------------------------------------------------------------------------------------------------------------*/
     function loadLocations(){
       aja()
       .url('locations')
@@ -81,7 +158,7 @@
                   "polyline": new google.maps.Polyline({
                         path: routes,
                         strokeColor: lineColor[i],
-                        strokeWeight: 1,
+                        strokeWeight: 2,
                         map: map
               })})
 
@@ -109,6 +186,23 @@
       .go();
     }
 
+    /*------------------------------------------------------------------------------------------------------------------
+    * FUNCTION: loadLocations
+    *
+    * DATE: March 27, 2017
+    * REVISIONS: (Date and Description)
+    *
+    * DESIGNER: Jackob Frank / Mark Tattrie / Deric Mccadden
+    * PROGRAMMER: Terry Kang
+    *
+    * INTERFACE: function loadLocations()
+    *
+    * RETURN: void
+    *
+    * NOTES:
+    *   Called when html document is ready and call a function to Initialize the google map. Then, via setInterval() method,
+    *   continue calling loadLocaions function to receive the stored locations from server.
+    ----------------------------------------------------------------------------------------------------------------------*/
     $(document).ready(function(){
       loadMap();
       loadLocations();
